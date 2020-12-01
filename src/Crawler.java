322925f4896fe633cf28d63613f92d08753e13b9
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2020-12-01 10:44:48 +0200

    Implementare schelet Crawler
    Issue:#5
    Am creat scheletul pentru clasa Crawler

---
+++ src/Crawler.java
@@ -0,0 +1,22 @@
+public class Crawler {
+    private String url;
+
+    public Crawler(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void dowload(String url){
+        
+    }
+    public void checkContents(){
+
+    }
+}
