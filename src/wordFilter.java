6b57363ae3319fdd24b4cc786b13154d6e07f67b
alexandraN <alexandranaicu98@gmail.com>
2020-12-07 11:32:57 +0200

    Implementare cautare dupa cuvant
    Issue:#31
    Am implementat clasa WordFilter, clasa mostenita din clasa Search care realizeaza cautarea unui keyFord dat ca parametru cand instantiem obiectul in toate fisierele dintr-un director, calea directorului este de asemenea data ca parametru cand instantiem obiectul de tipul clasei

---
+++ src/wordFilter.java
@@ -0,0 +1,55 @@
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class wordFilter implements Search {
+    private String path;
+    private String keyWord;
+     public wordFilter(String keyWord, String path )
+     {
+         this.keyWord=keyWord;
+         this.path=path;
+
+     }
+     @Override
+    public void search_method() {
+         File directory = new File(path);
+         if (directory == null || !directory.exists()) {
+             System.out.println("Directory doesn't exists!");
+         }
+         else
+         {
+            Pattern patern = Pattern.compile(keyWord);
+             ArrayList<String> filelist = new ArrayList<String>(); //lista de fisiere in care avem cuvantul
+             File[] files=directory.listFiles();
+
+             for (File f : files) {
+                 try {
+                     Scanner scanfFile = new Scanner(f);
+                     while(null != scanfFile.findWithinHorizon(patern,0))
+                     {
+
+                         MatchResult mr = scanfFile.match();
+                         filelist.add(f.getName());
+                         break;//ca sa nu mi afiseze de mai multe ori fisierul in care se gaseste cuvantul
+                     }
+
+                 }
+                 catch (Exception e) {
+                    // System.out.print("Eroare\n");
+                 }
+
+             }
+
+             System.out.println("Fisierele in care se gaseste cuvantul sunt: " + filelist);
+
+         }
+     }
+}
