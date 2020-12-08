751b79bdef7749e880e7151529b675c0fc2c283a
0x435446 <0x435446@gmail.com>
2020-12-08 17:05:47 +0200

    Implementare schelet siteMap
    Issue#4
    Implementeaza scheletul clasei ce va fi folosita la sitemap.

---
+++ src/SiteMap.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+class SiteMap {
+    List<Route> Paths;
+    private static SiteMap single_instance = null;
+    private SiteMap()
+    {
+    }
+    public static SiteMap getInstance()
+    {
+    }
+
+    void addRoute(Route component) {
+    }
+
+}
