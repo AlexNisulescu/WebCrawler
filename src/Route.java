fa37b4459fe369ace938d65374282c3a2f4a8e52
0x435446 <0x435446@gmail.com>
2020-12-10 16:25:41 +0200

    Implementare functionalitati Route
    Issue:#55
    Am implementat functionalitatea clasei Route, ce va fi folosita de catre Sitemap

--- src/Route.java
+++ src/Route.java
@@ -1,18 +1,58 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Route {
-    private List<String> content;
+    private ArrayList<String> content;
+    private String root;
+    private String parrent;
+    private int level;
 
-    public Route(List<String> content) {
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getParrent() {
+        return parrent;
+    }
+
+    public void setParrent(String parrent) {
+        this.parrent = parrent;
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+        this.content=new ArrayList<String>();
+    }
+
+    public Route(String root,String parrent) {
+        this.setRoot(root);
+        this.setParrent(parrent);
     }
 
     public List<String> getContent() {
         return content;
     }
 
-    public void setContent(List<String> content) {
+    public void setContent(ArrayList<String> content) {
+        this.content = content;
     }
 
     public void addContent(String path){
+        this.content.add(path);
     }
+
+    public void getAllContent(){
+        for(int i=0;i<content.size();i++){
+            System.out.print(getContent().get(i));
+        }
+    }
+
 }
