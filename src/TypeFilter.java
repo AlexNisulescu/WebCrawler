2a8dcd2d7815ab2ca9c54c6d1ec9e89b74b5ae53
alexandraN606 <73330374+alexandraN606@users.noreply.github.com>
2020-12-10 17:27:46 +0200

    Update TypeFilter.java

--- src/TypeFilter.java
+++ src/TypeFilter.java
@@ -13,12 +13,12 @@ public class TypeFilter implements Search{
     @Override
     public void search_method() {
     File directory = new File(this.path);
-    if(!directory.exists())
+    if(!directory.exists()) //verificam daca nu  exista calea catre acel director si afisam un mesaj corespunzator
     {
-        System.out.println(String.format("Directory %s does not exist", this.path));
+        System.out.println(String.format("Directory %s does not exist", this.path)); //verificam daca exista calea catre acel director
         return;
     }
-        if (!directory.isDirectory())
+        if (!directory.isDirectory())//verificam daca calea  nu este a unui  director
         {
             System.out.println(String.format("Provided value %s is not a directory", this.path));
             return;
@@ -28,17 +28,17 @@ public class TypeFilter implements Search{
       FileFilter allfiles = new FileFilter() {
           @Override
           public boolean accept(File pathname) {
-              if(pathname.getName().endsWith(type))
+              if(pathname.getName().endsWith(type))//verificam extensiile
               {
                   return true;
               }
               return false;
           }
       };
-        File[] files = directory.listFiles(allfiles);
+        File[] files = directory.listFiles(allfiles);//realizam o lista cu toate fisierele din director
         for (File f: files)
         {
-            System.out.println(f.getName());
+            System.out.println(f.getName());//le afisam pe cele care indeplinesc conditia
         }
     }
 }
