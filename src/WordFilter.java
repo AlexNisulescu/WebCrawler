5a4b21b2c596ac16134b9b35d1f261d7c282deb6
alexandraN606 <73330374+alexandraN606@users.noreply.github.com>
2020-12-11 16:20:42 +0200

    Update WordFilter.java

--- src/WordFilter.java
+++ src/WordFilter.java
@@ -7,12 +7,32 @@ import java.util.Scanner;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.IOException;
 
+/**
+ *
+ * This class implements the WordFilter that searchs by keyWord
+ * We display the files in the directory that contain the keyWord
+ * @author Alexandra Naicu
+ */
 
 public class WordFilter implements Search {
 
     private String path;
     private String keyWord;
+    
+    /**
+     *
+     * @param keyWord we are looking for in the files
+     */
+    
+    /**
+     *
+     * @param path the directory path from where 
+     *             we search and display the files
+     */
+    
+    
      public WordFilter(String keyWord, String path )
      {
          this.keyWord=keyWord;
@@ -20,7 +40,15 @@ public class WordFilter implements Search {
 
      }
      @Override
-    public void search_method() {
+    
+    /**
+     *
+     * This function is used to search the keyWord and desplay all files 
+     * that contain that pattern
+     * @throws FileNotFoundException if the file can't be opened
+     */
+    
+    public void search_method() throws FileNotFoundException{
          File directory = new File(path);
          if (directory == null || !directory.exists()) {
              System.out.println("Directory doesn't exists!");
@@ -28,7 +56,7 @@ public class WordFilter implements Search {
          else
          {
             Pattern patern = Pattern.compile(keyWord);
-             ArrayList<String> filelist = new ArrayList<String>(); //lista de fisiere in care avem cuvantul
+             ArrayList<String> filelist = new ArrayList<String>(); //file list
              File[] files=directory.listFiles();
 
              for (File f : files) {
@@ -39,17 +67,19 @@ public class WordFilter implements Search {
 
                          MatchResult mr = scanfFile.match();
                          filelist.add(f.getName());
-                         break;//ca sa nu mi afiseze de mai multe ori fisierul in care se gaseste cuvantul
+                         break;//to display only once
                      }
 
                  }
-                 catch (Exception e) {
-                    // System.out.print("Eroare\n");
-                 }
+              catch(IOException e){
+                //  System.out.println("An error occurred.");
+                //   e.printStackTrace();
+                //   throw new FileNotFoundException("The file you are trying to open " + "doesn't exist...");
+        }
 
              }
 
-             System.out.println("Fisierele in care se gaseste cuvantul sunt: " + filelist);
+             System.out.println("Fisierele in care se gaseste cuvantul sunt: " + filelist); //print all files have that type
 
          }
      }
