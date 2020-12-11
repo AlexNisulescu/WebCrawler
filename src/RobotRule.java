6c7eac6b1ee59399ed24281477b39e163e10c6ad
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2020-12-11 09:25:37 +0200

    Implementare Functionalitate CheckContents()
    issue:#65
    Am implementat metoda checkContents care verifica daca site-ul respectiv poate fi descarcat. Pentru aceasta am mai creat si clasa RobotRule.java. Totodata am updatat si .gitignore sa ignore fisierele .txt si .cnf

---
+++ src/RobotRule.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * This class is a helper for the Crawler class
+ * All it does is override the toString() method
+ * @author Alexandru Nisulescu
+ */
+public class RobotRule
+{
+    public String userAgent;
+    public String rule;
+
+    RobotRule() {
+
+    }
+
+    @Override public String toString()
+    {
+        StringBuilder result = new StringBuilder();
+        String NEW_LINE = System.getProperty("line.separator");
+        result.append(this.getClass().getName() + " Object {" + NEW_LINE);
+        result.append("   userAgent: " + this.userAgent + NEW_LINE);
+        result.append("   rule: " + this.rule + NEW_LINE);
+        result.append("}");
+        return result.toString();
+    }
+}
