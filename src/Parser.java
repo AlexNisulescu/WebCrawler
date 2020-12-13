0558e9bc2e161588e6c8ad068177e99f11881e86
Cosmina15 <73330356+Cosmina15@users.noreply.github.com>
2020-12-13 10:59:22 +0200

    Update Parser.java
    Am modificat clasa Parser astfel: fisierul html este citit de la un path

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import  java.util.ArrayList;
@@ -9,107 +12,109 @@ import  java.util.ArrayList;
  */
 public class Parser
 {
-    private String title;
-    private String body;
-    final private String htmlPage;
-    private ArrayList<String> urlList;
-
-    public Parser(String htmlContent) {
-        this.htmlPage = htmlContent;
-    }
-
+    private String htmlPath;
     /**
      * This method helps parser object to take the source from which information is extracted.
-     * @param htmlContent HTML page content
+     * @param htmlPath path to HTML page
      * @throws BadArgumentsException
      */
-    public void parse(String htmlContent) throws BadArgumentsException {
-        Parser parser = new Parser(htmlContent);
-        this.urlList = getURLs(parser.htmlPage);
-        this.title = getTitle(parser.htmlPage);
-        this.body = getBody((parser.htmlPage));
+    public void parse(String htmlPath)
+    {
+        this.htmlPath = htmlPath;
     }
 
     /**
-     * The method gets the title from HTML content. It uses regex to match the title.
-     * @param htmlContent HTML page content
-     * @return the title found
-     * @throws BadArgumentsException
+     * This method gets content from HTML page from a given path
+     * @return
+     * @throws FileNotFoundException
      */
-    public String getTitle(String htmlContent) throws BadArgumentsException {
+    public String getStringContent() throws FileNotFoundException {
+        try {
+            StringBuilder contentBuilder = new StringBuilder();
+            BufferedReader in = new BufferedReader(new FileReader(this.htmlPath));
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+            }
+            in.close();
+            String htmlContent = contentBuilder.toString();
+            return htmlContent;
+        } catch(Exception e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            throw new FileNotFoundException("The file does not exist");
+        }
+    }
+    /**
+     * The method gets the title from HTML content. It uses regex to match the title.
+     * @return the title found
+     */
+    public String getTitle(){
         String result = null;
         try
         {
             Pattern pattern  = Pattern.compile("\\<title>(.*)\\</title>", Pattern.CASE_INSENSITIVE|Pattern.DOTALL);
-            Matcher matcher = pattern.matcher(htmlContent);
+            Matcher matcher = pattern.matcher(getStringContent());
             while (matcher.find())
             {
                 result = matcher.group(1);
             }
-            return result;
+
         }
         catch(Exception e){
             System.out.println("An error occurred.");
             e.printStackTrace();
-            throw new BadArgumentsException("Bad argument for extract title");
+
         }
+        return result;
     }
 
     /**
      * The method gets the body from HTML content. It uses regex to match the body.
-     * @param htmlContent HTML page content
      * @return the body found
-     * @throws BadArgumentsException
      */
-    public String getBody( String htmlContent ) throws BadArgumentsException {
+    public String getBody(){
         String result = null;
         try
         {
             Pattern pattern  = Pattern.compile(".*?<body.*?>(.*?)</body>.*?", Pattern.DOTALL);
-            Matcher matcher = pattern.matcher(htmlContent);
+            Matcher matcher = pattern.matcher(getStringContent());
             while (matcher.find())
             {
                 result = matcher.group(1);
             }
-            return result;
         }
         catch (Exception e)
         {
             System.out.println("An error occurred.");
             e.printStackTrace();
-            throw new BadArgumentsException("Bad argument for extract body");
         }
+        return result;
     }
 
     /**
      * The method gets the URLs from HTML content. It uses regex to match the URLs.
-     * @param htmlContent HTML page content
      * @return an array of URLs
-     * @throws BadArgumentsException
      */
-    public ArrayList<String> getURLs(String htmlContent) throws BadArgumentsException {
-        try
-        {
+    public ArrayList<String> getURLs() {
+        ArrayList<String> result = new ArrayList<String>();
+        try {
+            String htmlContent = getStringContent();
             String patternString = "<a\\s+href\\s*=\\s*(\"[^\"]*\"|[^\\s>]*)\\s*>";
             Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
             Matcher matcher = pattern.matcher(htmlContent);
-            ArrayList<String> result = new ArrayList<String>();
 
-            while (matcher.find())
-            {
+            while (matcher.find()) {
                 int start = matcher.start();
                 int end = matcher.end();
                 String url = htmlContent.substring(start, end);
                 result.add(url);
             }
 
-            return result;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
-            throw new BadArgumentsException("Bad argument for extract URLs");
         }
+        return result;
     }
 }
